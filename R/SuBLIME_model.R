b2eb1160be5744da708e755bccb69c90280b442e
muschellij2 <muschellij2@gmail.com>
2016-09-29 16:22:36 -0400

    Passes R CMD check

--- R/SuBLIME_model.R
+++ R/SuBLIME_model.R
@@ -1,12 +1,9 @@
 #' @title Sublime Predictive model
 #'
 #' @description Predictive model for SuBLIME algorithm
-#' @name SuBLIME_model
-#' @docType data
 #' @usage SuBLIME_model
 #' @format An \code{lm} object, but with data an other things, notably 
 #' qr removed
 #' @references Sweeney, E. M., et al. "Automatic lesion incidence estimation and detection in multiple sclerosis using multisequence longitudinal MRI." American Journal of Neuroradiology 34.1 (2013): 68-73.
-#' @keywords datasets
-NULL
+"SuBLIME_model"
 
